a99db5f2720e39245f0ad370b7410ff117036fe4
Dmitry Stupin <dm.stup@mail.ru>
2026-09-06 00:22:05 +0700

    feat: enhance recordform with calendar toggle functionality

--- src/components/RecordForm/RecordForm.tsx
+++ src/components/RecordForm/RecordForm.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/popover.tsx";
 import {Calendar} from "@/components/ui/calendar.tsx";
 import {format} from "date-fns";
+import {useState} from "react";
 
 type RecordFormProps = {
   inputText: string
@@ -18,14 +19,18 @@ type RecordFormProps = {
 
 const RecordForm = (props: RecordFormProps) => {
   const {inputText, selectedDate, setInputText, setSelectedDate, addNewRecord} = props
+  const [openCalendar, setOpenCalendar] = useState(false)
 
   return (
     <div className={'flex flex-col gap-3 md:flex-row'}>
       <Input className={'h-12 text-base '} placeholder={'Введите текст'} value={inputText} onChange={(event) => setInputText(event.target.value)} />
-      <Popover>
+      <Popover open={openCalendar} onOpenChange={setOpenCalendar}>
         <PopoverTrigger render={<Button className={'h-12 text-base'} variant={'outline'}>{selectedDate ? format(selectedDate, 'dd.MM.yyyy') : <span>Выберите дату</span>}</Button>} />
         <PopoverContent>
-          <Calendar mode={"single"} selected={selectedDate} onSelect={setSelectedDate} />
+          <Calendar mode={"single"} selected={selectedDate} onSelect={(date) => {
+            setSelectedDate(date)
+            setOpenCalendar(false)
+          }} />
         </PopoverContent>
       </Popover>
       <Button className={'h-12 text-base'} onClick={addNewRecord}>Добавить новую запись</Button>
