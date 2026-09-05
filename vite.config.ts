38abc8ad6d5238aca9cb2ec7a9f7cb37d633c48c
Dmitry Stupin <dm.stup@mail.ru>
2026-09-05 23:58:45 +0700

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/noura/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
